0e790013093b8eaa42327e1df5fa765c58d10105
Simon Richardson <simon.richardson@smhi.se>
2022-12-07 08:36:35 +0100

    day_7

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -2,6 +2,9 @@ input_file = open("input.txt", "r")
 content = input_file.readlines()
 input_file.close()
 
+part1 = 0
+dir_sizes = []
+
 def sum_func(file_sys):
   global part1
   total_size = 0
@@ -11,10 +14,26 @@ def sum_func(file_sys):
       total_size += x
     else:
       total_size += sum_func(x)
+
   if total_size <= 100000:
     part1 = part1 + total_size
+#    dir_sizes.append(total_size)
+
+  else:
+    dir_sizes.append(total_size)
+
   return total_size
 
+def find_small_dir(file_sys, required_size):
+  global dir_sizes
+  for x in file_sys.values():
+    if isinstance(x, int):
+      if x >= required_size:
+        dir_sizes.append(x)
+    else:
+      find_small_dir(x, required_size)
+
+  return dir_sizes
 
 file_system = {}
 cf = [file_system]
@@ -41,14 +60,18 @@ for cmd in content:
     name = cmd[1]
     cf[-1][name] = int(size)
 
-print(file_system)
-#print(cf)
-
-
-
-
-
-
 root_size = sum_func(file_system)
+required_space = 30000000 - (70000000 - root_size)
 
-print(part1_size)
+#print(part1)
+
+print(f"Root size: {root_size}")
+print(f"Required space: {required_space}")
+
+
+dir_sizes.sort()
+
+for size in dir_sizes:
+  if size >= required_space:
+    print(f"Smallest directory: {size}")
+    break
